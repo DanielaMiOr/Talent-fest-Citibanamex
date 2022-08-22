4fff1c8a7d764a5e35d6173d1728841a7b3aab03
veroguzrob72 <vgrdesd@gmail.com>
2022-08-22 16:41:15 -0500

    Initialize app

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBoHIY_nN1naSwYZ2YS8dkjgkBJLVAvBIs",
+  authDomain: "talent-fest-citibanamex.firebaseapp.com",
+  projectId: "talent-fest-citibanamex",
+  storageBucket: "talent-fest-citibanamex.appspot.com",
+  messagingSenderId: "455838549243",
+  appId: "1:455838549243:web:bf85b51a32efc25c665c5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
